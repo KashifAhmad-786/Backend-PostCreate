64d7958e15edf18a62c6c01bc249ad343ad68849
kashif <ka8285900@email.com>
2025-11-28 23:31:27 +0500

    boss

---
+++ models/post.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+
+const postSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
+    Date: { type: Date, default: Date.now },
+    content: String,
+    likes:[
+        { type: mongoose.Schema.Types.ObjectId, ref: 'user' }
+    ]
+});
+module.exports = mongoose.model('post', postSchema);
